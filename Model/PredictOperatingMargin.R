396f2ff0fa73fa9dcd16133012e6233a42bb804c
Prabhat-MSFT <ptrip@microsoft.com>
2017-02-01 03:12:39 +0000

    Add files via upload

---
+++ Model/PredictOperatingMargin.R
@@ -0,0 +1,69 @@
+usePackage <- function(p) {
+  if (!is.element(p, installed.packages()[,1]))
+    install.packages(p, dep = TRUE)
+  library(p, character.only = TRUE)
+}
+
+usePackage("curl")
+usePackage("ggplot2")
+usePackage("mrsdeploy")
+
+x <- read.csv(curl("https://raw.githubusercontent.com/Prabhat-MSFT/R-Server-Operationalization/master/TrainingData.csv"))
+head(x,5)
+
+
+myModel <- rxGlm(Margin ~ TotalRooms
+                + NearestCompetition
+                + AnnualVisitors
+                + MedianIncome, data = x)
+
+
+CheckOperatingMargin <- function(i_totalrooms,i_nearestcompetition,i_annualvisitor,i_medianincome)
+{
+  myInput <- data.frame(TotalRooms = i_totalrooms,
+                        NearestCompetition = i_nearestcompetition,
+                        AnnualVisitors = i_annualvisitor,
+                        MedianIncome = i_medianincome
+  )
+  
+  prediction <- rxPredict(modelObject = myModel, data = myInput) 
+  margin <- prediction$Margin_Pred
+}
+
+print(CheckOperatingMargin(1000,5,500000, 75))
+
+remoteLogout()
+remoteLogin("http://40.86.94.151:12800", session = TRUE, diff = TRUE, commandline =  TRUE, 
+            prompt = "REMOTE_TECHREADY>>>", username = "admin", password = "Audi@2015")
+pause()
+
+
+# Show API capabilities
+api$capabilities()
+
+#Consume the service
+results <- api$CheckOperatingMargin(3400, 2, 50000, 75)
+results$output("margin")
+service_name <- "CheckLocationViabilityForHotel"
+
+# Publish service
+api <- publishService(
+  service_name,
+  code = CheckOperatingMargin,
+  model = myModel,
+  inputs = list(TotalRooms = 'numeric', NearestCompetition = 'numeric', AnnualVisitors = 'numeric', MedianIncome = 'numeric'),
+  outputs = list(margin = 'numeric'),
+  v = 'v1.0.0')
+
+#List all services
+services <- listServices()
+services
+
+#Generate swagger json file
+cat(api$swagger(), file = "C:/Users/azureuser/Documents/TechReady/WhereToOpenHotel/HotelOperatingMargin.json")
+
+cap <- api$capabilities()
+cap
+cap$swagger
+
+
